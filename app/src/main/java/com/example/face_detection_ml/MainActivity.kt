fc3964c83cc010bff44af70d56e966a5f842a989
M.A <md.ali2001@hotmail.com>
2023-03-05 03:17:19 -0800

    Initialized project with firebase ML-toolkit face_detection

---
+++ app/src/main/java/com/example/face_detection_ml/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.face_detection_ml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.mlkit.vision.face.FaceDetectorOptions
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // High-accuracy landmark detection and face classification
+        val highAccuracyOpts = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+// Real-time contour detection
+        val realTimeOpts = FaceDetectorOptions.Builder()
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+            .build()
+    }
+}
